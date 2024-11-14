1a5cdef842ea251a53deb09201b96c07960d3321
Mmapaseka Letlhage <mmapasekaletlhage@gmail.com>
2024-11-14 09:06:55 +0200

    Added some css and icon

--- frontend/README.md
+++ frontend/README.md
@@ -10,3 +10,7 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+Mmapaseka
+Updated the website icon and title in the index.html file
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -8,10 +8,14 @@ function Header({ handleTipClick }) {
       <h2>Avoid cramming and stress! Learn new material, well.</h2>
       <p>
         Did you know that taking breaks and getting rest are part of learning?
-        Let us know what you'd like to understand. We'll give you the content
-        you need and the study tools to actuall learn.
       </p>
-      <button onClick={handleTipClick}>Get a tip for studying well</button>
+      <p>
+        Let us know what you'd like to understand. We'll give you the content
+        you need and the study tools to actually learn.
+      </p>
+      <button onClick={handleTipClick} className="button">Get Started</button>
+
+      {/* <button onClick={handleTipClick}>Get a tip for studying well</button> */}
     </header>
   );
 }
