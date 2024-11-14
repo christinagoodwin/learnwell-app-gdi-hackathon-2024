104c27d251ac25cc4a47da752e072fc052413b3c
Gardenqua <qjgarden1@gmail.com>
2024-11-14 12:45:16 -0500

    openai_integration chaneges

--- backend/learnwell_backend/learnwell_backend/urls.py
+++ backend/learnwell_backend/learnwell_backend/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user_functionality.urls')),
-    path('open_ai/', include('openai_integration.urls')),
     
 ]
