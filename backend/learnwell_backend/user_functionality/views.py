234cc88e26c02b7e07d6aa68a7d3ffbef974af8e
Gardenqua <qjgarden1@gmail.com>
2024-11-15 08:06:13 -0500

    Additional Changes to the AI impemenatation

--- backend/learnwell_backend/user_functionality/views.py
+++ backend/learnwell_backend/user_functionality/views.py
@@ -18,11 +18,10 @@ class TopicChoiceView(APIView):
 class TopicOverviewView(APIView):
     def get(self, request):
 
-        fakequery= "Learning is the process of acquiring new understanding, knowledge, behaviors, skills, values, attitudes, and preferences.[1] The ability to learn is possessed by humans, non-human animals, and some machines; there is also evidence for some kind of learning in certain plants.[2] Some learning is immediate, induced by a single event (e.g. being burned by a hot stove), but much skill and knowledge accumulate from repeated experiences.[3] The changes induced by learning often last a lifetime, and it is hard to distinguish learned material that seems to be  from that which cannot be retrieved.[4]"
-        # Placeholder response
-        query=summarize_text(fakequery, max_tokens=180)
+        
+        query=summarize_text(request, max_tokens=180)
         response_data = {
-            "original_text": fakequery,
+            "original_text": request,
             "summary": query,
         }
 
