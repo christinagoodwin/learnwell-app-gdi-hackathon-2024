d4db4558b7d11e8da40d49f6ed62f44589363672
Mmapaseka Letlhage <mmapasekaletlhage@gmail.com>
2024-11-15 12:56:59 +0200

    Completed the homepage

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -4,9 +4,21 @@ import ButtonLink from "../ButtonLink/ButtonLink";
 import { Outlet } from "react-router-dom";
 
 function Main({ handleTipClick }) {
+  const handleGetStartedClick = () => {
+    navigate("/next");
+  };
+
   return (
-    <main>
-      <h2>Avoid cramming and stress! Learn new material, well.</h2>
+    <main className="mainContent">
+      <h1>Avoid cramming and stress! Learn new material effectively:</h1>
+      <h2>PICK A QUITE, FAMILIAR PLACE.</h2>
+      <h2>AVOID DISTRACTIONS (E.G, PUT PHONE ON SILENT).</h2>
+      <h2>BE RESTED AND HYDRATED</h2>
+      <h2>GATHER MATERIALS (E.G, PEN, PAPER, WATER)</h2>
+      <button onClick={handleTipClick}>Get another tip for studying well.</button>
+      <h1></h1>
+      <ButtonLink className="btn" type="button" to="/picktopic">Get Started</ButtonLink>
+      {/* <h2>Avoid cramming and stress! Learn new material, well.</h2>
       <p>
         Did you know that taking breaks and getting rest are part of learning?
         Let us know what you'd like to understand. We'll give you the content
@@ -26,13 +38,11 @@ function Main({ handleTipClick }) {
           If you're in a social environment, buddy up with someone else who is
           studying.
         </li>
-      </ul>
-      <button onClick={handleTipClick}>
-        Get another tip for studying well.
-      </button>
-      <ButtonLink type="button" to="/picktopic" className="footer-btn">
+      </ul> */}
+      
+      {/* <ButtonLink type="button" to="/picktopic" className="footer-btn">
         I'm ready to pick my topic.
-      </ButtonLink>
+      </ButtonLink> */}
       <Outlet />
     </main>
   );
